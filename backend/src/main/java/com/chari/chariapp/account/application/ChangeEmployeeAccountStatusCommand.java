508d729ca995decc18b948411c9be1a2632a3860
Mohammed Mustafa <mohammedmaher6000@gmail.com>
2026-09-19 18:51:36 +0300

    Added endpoints for passports and nationalid and birthcertificate

--- backend/src/main/java/com/chari/chariapp/account/application/ChangeEmployeeAccountStatusCommand.java
+++ backend/src/main/java/com/chari/chariapp/account/application/ChangeEmployeeAccountStatusCommand.java
@@ -3,5 +3,6 @@ package com.chari.chariapp.account.application;
 import com.chari.chariapp.account.domain.AccountId;
 import com.chari.chariapp.account.domain.AccountStatus;
 
+
 public record ChangeEmployeeAccountStatusCommand(AccountId administratorId, AccountId employeeId, AccountStatus status) {
 }
